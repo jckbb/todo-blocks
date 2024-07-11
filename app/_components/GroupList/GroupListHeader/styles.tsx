7e9eb41a288fca68138526969bf0e9651205dcb1
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-07-11 14:20:43 -0400

    refactor file structure

---
+++ app/_components/GroupList/GroupListHeader/styles.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  header: {
+    paddingBottom: 24,
+  },
+  navigateToTodoButton: {
+    flex: 1,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#2f3542",
+  },
+  navigateToTodoLink: {
+    flex: 1,
+    textAlign: "center",
+    color: "white",
+  },
+});

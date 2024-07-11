37038a5c0e297ebb95a8eea3a765d44ec3555e71
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-07-11 15:16:32 -0400

    abstract types and refactor task field

--- app/_components/GroupList/index.tsx
+++ app/_components/GroupList/index.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 import { View, Text, SectionList } from "react-native";
 import styles from "./styles";
-
-type Task = {
-  task: string;
-};
-
-type TodoGroup = {
-  title: string;
-  data: Task[];
-  color?: string;
-};
+import { Task, TodoGroup } from "@/app/_machines/taskMachine";
 
 interface Props {
   groups: TodoGroup[];
@@ -32,7 +23,7 @@ const GroupList = ({ groups, listHeader }: Props) => {
       ]}
     >
       <View style={styles.item}>
-        <Text>{item.task}</Text>
+        <Text>{item.description}</Text>
       </View>
     </View>
   );

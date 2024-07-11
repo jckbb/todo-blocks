fd0c3f3d5a0898e9e7af71f76ba4bfcaa41c16a3
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-07-11 14:18:30 -0400

    refactor stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(todo)/index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="createTodoModal/index"
+          options={{ headerShown: false, presentation: "modal" }}
+        />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>

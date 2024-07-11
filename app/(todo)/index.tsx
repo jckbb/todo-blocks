f82f29e55bb4de7d9b1f26c1fcfd5c1e1fb748c7
joseph burghard <josephburghard@josephs-MBP.lan>
2024-07-11 10:51:46 -0400

    add dummy data and groulist component

--- app/(todo)/index.tsx
+++ app/(todo)/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import GroupList from "./components/GroupList";
+
+const styles = StyleSheet.create({
+  mainContainer: { flex: 1 },
+});
+
+const dummyGroups = [
+  {
+    title: "food",
+    data: [{ task: "wholefoods run" }, { task: "fish" }],
+    color: "#7bed9f",
+  },
+  {
+    title: "errands",
+    data: [{ task: "walk dog" }, { task: "takeout trash" }],
+    color: "#70a1ff",
+  },
+  {
+    title: "other",
+    data: [{ task: "drink water" }],
+  },
+];
 
 const Todo = () => {
   return (
-    <SafeAreaView>
-      <Text>{"todo page"}</Text>
+    <SafeAreaView style={styles.mainContainer}>
+      <GroupList groups={dummyGroups} />
     </SafeAreaView>
   );
 };

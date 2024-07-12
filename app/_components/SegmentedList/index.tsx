eb1825659392225edd47d147fb73f960cbbb10d0
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-07-11 21:09:07 -0400

    update component with state and refactor field

--- app/_components/SegmentedList/index.tsx
+++ app/_components/SegmentedList/index.tsx
@@ -1,27 +1,32 @@
 import { View } from "react-native";
 import SegmentedButton from "./components/SegmentedButton";
 import styles from "./styles";
+import { useState } from "react";
 
-export type Option = { label: string; color?: string };
+export type Option = { name: string; color: string };
 
 type SegmentedList = {
-  onChange: (value: string) => void;
+  onChange: (group: { name: string; color: string }) => void;
   options: Option[];
-  selected: string;
 };
 
-const SegmentedList = ({ options, onChange, selected }: SegmentedList) => (
-  <View style={styles.segmentedList}>
-    {options.map((item, index) => (
-      <SegmentedButton
-        label={item.label}
-        color={item.color}
-        key={index}
-        isSelected={selected === item.label}
-        onPress={() => onChange(item.label)}
-      />
-    ))}
-  </View>
-);
-
+const SegmentedList = ({ options, onChange }: SegmentedList) => {
+  const [selected, setSelected] = useState<string>();
+  return (
+    <View style={styles.segmentedList}>
+      {options.map((item, index) => (
+        <SegmentedButton
+          label={item.name}
+          color={item.color}
+          key={index}
+          isSelected={selected === item.name}
+          onPress={() => {
+            setSelected(item.name);
+            onChange({ name: item.name, color: item.color });
+          }}
+        />
+      ))}
+    </View>
+  );
+};
 export default SegmentedList;
